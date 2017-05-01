2d925fa2b4007c5a4ceb5b938aff43829956dcd3
mvanpatter <michaelvanpatter@gmail.com>
2017-04-30 21:07:07 -0400

    Latest
    Attempts to clean up the front-page file

--- front-page.php
+++ front-page.php
@@ -1,18 +1,18 @@
 <?php get_template_part('partials/head'); ?>
 <html>
 
-<!-- Background image -->
 
-  <?php $page = get_page_by_title ( 'Welcome' ); ?>
-  <?php if ( $background = wp_get_attachment_image_src( get_post_thumbnail_id( $page->ID ), 'full' ) ) : ?>
-
-    <div style="background: url('<?php echo $background[0]; ?>') top right no-repeat; height:900px;" id="welcome-intro" class="clear">
-
-  <?php endif; ?>
 
 <body <?php body_class(); ?>>
 
 
+
+<!-- Retrieves a post given its title -->
+
+    <?php $page = get_page_by_title ( 'Welcome' ); ?>
+
+    <div class="square">
+    </div>
 
 <?php // get_template_part('partials/header'); ?>
 
@@ -20,15 +20,12 @@
 
 
 
-    <div class="main-image-container hero">
-
-    </div>
-
     <div class="main-content">
         <div class="main-content-text"
           <?php the_content(); ?>
         </div>
     </div>
+
 
 <?php endwhile; else : ?>
 	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
